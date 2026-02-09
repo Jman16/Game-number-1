2656075625759ea5e18bd193b67ccb39c516109e
Jman16 <popij053@hsd.k12.or.us>
2026-02-09 10:01:43 -0800

    add rollt ball

---
+++ Assets/Scripts/Plaerconteroll.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Plaerconteroll : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+    void OnMove() {
+      // Functoin body 
+      }
+}
+    
+   
